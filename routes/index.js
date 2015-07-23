e90855d3bffcd64393d3d05daac670dbc0cc8bd5
iosu <iosu_9207@hotmail.com>
2015-07-23 18:47:10 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId',quizController.load);
+
+//Definicion rutas de /quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 
+//Definicion rutas de /author
 router.get('/author', function(req, res, next) {
   res.render('author');
 });
